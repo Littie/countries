fcc3cd815c24079e061d0aabee9d5381d28408f4
Artem <polikarpov_aj@groupbwt.com>
2017-05-25 02:11:07 +0300

    Minor refact

--- resources/views/countries/show.blade.php
+++ resources/views/countries/show.blade.php
@@ -2,28 +2,29 @@
 
 @section('content_area')
     <h1>{{ $country->getAttribute('name') }}</h1>
-    <h3>Cities</h3>
+    <h3>{{ trans('countries.show.city_header') }}</h3>
     <ul>
         @if (count($cities))
             @foreach($cities as $city)
                 <li>{{ $city->getAttribute('name') }}</li>
             @endforeach
         @else
-            <h3>Empty cities</h3>
+            <h3>{{  trans('countries.show.empty') }}</h3>
         @endif
     </ul>
-    <h3>Languages</h3>
+    <h3>{{ trans('countries.show.languages_header') }}</h3>
     <ul>
         @if (count($languages))
             @foreach($languages as $language)
                 <li>{{ $language->getAttribute('name') }}</li>
             @endforeach
         @else
-            <h3>Empty languages</h3>
+            <h3>{{  trans('countries.show.empty') }}</h3>
         @endif
     </ul>
     <div class="pull-right">
-        <a href="{{ route('cities.create', ['country' => $country]) }}" class="btn btn-primary btn-success">Add city</a>
+        <a href="{{ route('cities.create', ['country' => $country]) }}"
+           class="btn btn-primary btn-success">{{  trans('countries.show.buttons.add') }}</a>
     </div>
-    <a href="{{ url()->previous() }}" class="btn btn-default">Back</a>
+    <a href="{{ url()->previous() }}" class="btn btn-default">{{  trans('countries.show.buttons.back') }}</a>
 @endsection
