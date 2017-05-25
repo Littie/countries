dc691d55177a1f65886efa6ac28d524736d8720b
Artem Polikarpov <a.polikarpov@amgrade.com>
2017-05-25 10:38:59 +0300

    Add trans for nav

---
+++ resources/lang/en/nav.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+    'countries' => 'Countries',
+    'cities'    => 'Cities',
+    'languages' => [
+        'languages' => 'Languages',
+        'check'     => 'Check language',
+    ],
+];
--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -1,5 +1,6 @@
 <div class="list-group">
-    <a class="list-group-item" href="{{ route('countries.index') }}">Countries</a>
-    <a class="list-group-item" href="{{ route('cities.index') }}">Cities</a>
-    <a class="list-group-item" href="{{ route('languages.index') }}">Languages</a>
+    <a class="list-group-item" href="{{ route('countries.index') }}">{{ trans('nav.countries') }}</a>
+    <a class="list-group-item" href="{{ route('cities.index') }}">{{ trans('nav.cities') }}</a>
+    <a class="list-group-item" href="{{ route('languages.index') }}">{{ trans('nav.languages.languages') }}</a>
+    <a class="list-group-item" href="{{ route('languages.index') }}">{{ trans('nav.languages.check') }}</a>
 </div>
